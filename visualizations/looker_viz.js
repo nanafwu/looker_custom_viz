ad786e8c8cc13158994a3008e0d9d85d172ccbe9
Nana Wu <wu.f.nana@gmail.com>
2021-09-09 21:25:09 -0700

    test viz

--- visualizations/looker_viz.js
+++ visualizations/looker_viz.js
@@ -6,22 +6,7 @@
  **/
 
 const visObject = {
- /**
-  * Configuration options for your visualization. In Looker, these show up in the vis editor
-  * panel but here, you can just manually set your default values in the code.
-  **/
-  options: {
-    first_option: {
-    	type: "string",
-      label: "My First Option",
-      default: "Default Value"
-    },
-    second_option: {
-    	type: "number",
-      label: "My Second Option",
-      default: 42
-    }
-  },
+
  
  /**
   * The create function gets called when the visualization is mounted but before any
@@ -37,63 +22,99 @@ const visObject = {
   * the data and should update the visualization with the new data.
   **/
 	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
+    
     // set the dimensions and margins of the graph
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    // set the ranges
-    var x = d3.scaleBand()
-              .range([0, width])
-              .padding(0.1);
-    var y = d3.scaleLinear()
-              .range([height, 0]);
+    var margin = {top: 30, right: 30, bottom: 30, left: 50},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    // append a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
-    element.innerHTML = ""
-    var svg = d3.select("#vis").append("svg")
+    var svg = d3.select("#my_dataviz")
+      .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
-        .attr("transform", 
+        .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")");
-    
-    formattedData = []
 
-    // format the data
-    data.forEach(function(d) {
-      formattedData.push({
-      	count: d["game.count"]["value"],
-        friendly_class: d["game.friendly_class"]["value"],
-        opponent_class: d["game.opponent_class"]["value"]
-      });
+    // get the data
+    d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_doubleHist.csv", function(data) {
+
+      // add the x Axis
+      var x = d3.scaleLinear()
+          .domain([-10,15])
+          .range([0, width]);
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      // add the y Axis
+      var y = d3.scaleLinear()
+                .range([height, 0])
+                .domain([0, 0.12]);
+      svg.append("g")
+          .call(d3.axisLeft(y));
+
+      // Compute kernel density estimation
+      var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(60))
+      var density1 =  kde( data
+          .filter( function(d){return d.type === "variable 1"} )
+          .map(function(d){  return d.value; }) )
+      var density2 =  kde( data
+          .filter( function(d){return d.type === "variable 2"} )
+          .map(function(d){  return d.value; }) )
+
+      // Plot the area
+      svg.append("path")
+          .attr("class", "mypath")
+          .datum(density1)
+          .attr("fill", "#69b3a2")
+          .attr("opacity", ".6")
+          .attr("stroke", "#000")
+          .attr("stroke-width", 1)
+          .attr("stroke-linejoin", "round")
+          .attr("d",  d3.line()
+            .curve(d3.curveBasis)
+              .x(function(d) { return x(d[0]); })
+              .y(function(d) { return y(d[1]); })
+          );
+
+      // Plot the area
+      svg.append("path")
+          .attr("class", "mypath")
+          .datum(density2)
+          .attr("fill", "#404080")
+          .attr("opacity", ".6")
+          .attr("stroke", "#000")
+          .attr("stroke-width", 1)
+          .attr("stroke-linejoin", "round")
+          .attr("d",  d3.line()
+            .curve(d3.curveBasis)
+              .x(function(d) { return x(d[0]); })
+              .y(function(d) { return y(d[1]); })
+          );
+
     });
 
-    // Scale the range of the data in the domains
-    x.domain(formattedData.map(function(d) { return d.friendly_class; }));
-    y.domain([0, d3.max(formattedData, function(d) { return d.count; })]);
+    // Handmade legend
+    svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
+    svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
+    svg.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
 
-    // append the rectangles for the bar chart
-    svg.selectAll(".bar")
-      .data(formattedData)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("style", "fill: #6c43e0;")
-      .attr("x", function(d) { return x(d.friendly_class); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.count); })
-      .attr("height", function(d) { return height - y(d.count); });
-
-    // add the x Axis
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    // add the y Axis
-    svg.append("g")
-      .call(d3.axisLeft(y));
+    // Function to compute density
+    function kernelDensityEstimator(kernel, X) {
+      return function(V) {
+        return X.map(function(x) {
+          return [x, d3.mean(V, function(v) { return kernel(x - v); })];
+        });
+      };
+    }
+    function kernelEpanechnikov(k) {
+      return function(v) {
+        return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+      };
+    }
 
 		doneRendering()
 	}
