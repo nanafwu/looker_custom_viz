f75965796bc2c4d6dffc94f1a3ee8d02be0f0b79
Nana Wu <wu.f.nana@gmail.com>
2021-09-12 18:20:37 -0700

    posterior probability calculation

--- visualizations/looker_viz5.js
+++ visualizations/looker_viz5.js
@@ -5,6 +5,29 @@
  *  - Example Visualizations - https://github.com/looker/custom_visualizations_v2/tree/master/src/examples
  **/
 
+function compare_conversion_probability (
+  alpha_posterior_A, beta_posterior_A, alpha_posterior_B, beta_posterior_B) {
+  var samples = 5000;
+  var A_greater_than_B = 0;
+  var B_greater_than_A = 0;
+    
+  var comparison_calculations = _.map(_.range(samples), function(i){ 
+    var sample_A = jStat.beta.sample(alpha_posterior_A, beta_posterior_A); 
+    var sample_B = jStat.beta.sample(alpha_posterior_B, beta_posterior_B); 
+    if (sample_A > sample_B) {
+      A_greater_than_B += 1;
+    } else {
+      B_greater_than_A += 1;
+    }  
+  }); 
+  
+  var prob_A_greater_than_B = A_greater_than_B / samples;
+  var prob_B_greater_than_A = B_greater_than_A / samples;
+    
+  console.log('A_greater_than_B', prob_A_greater_than_B,
+             'B_greater_than_A', prob_B_greater_than_A)  
+}
+
 looker.plugins.visualizations.add({
 
  options: {
@@ -19,6 +42,7 @@ looker.plugins.visualizations.add({
       default: 42
     }
   },
+  
  /**
  /**
   * The create function gets called when the visualization is mounted but before any
@@ -36,29 +60,40 @@ looker.plugins.visualizations.add({
   **/
   updateAsync: function(data, element, config, queryResponse, details, doneRendering){
     console.log('-- Update Async --')
-    console.log('data: ', data),
+    //console.log('data: ', data),
     console.log('config: ', config)
-    console.log('details', details),
+    //console.log('details', details),
     console.log('queryResponse', queryResponse)
 
-    alpha_prior = 1;
-    beta_prior = 1;
+    var alpha_prior = 1;
+    var beta_prior = 1;
+    var variant_A_label = 'Variant A'; 
+    var variant_B_label = 'Variant B';
+
+    var visitors_to_A = 1300;
+    var visitors_to_B = 1275;
     
-    visitors_to_A = 1300;
-    visitors_to_B = 1275;
+    var conversions_from_A = 120;
+    var conversions_from_B = 125;
     
-    conversions_from_A = 120;
-    conversions_from_B = 125;
+    // for(var dim of queryResponse.fields.dimensions) {
+    //   if (!variant_A_label && dim.type === "string") {
+        
+    //   }  
+    // }
+   
     
-    alpha_posterior_A = alpha_prior + conversions_from_A;
-    beta_posterior_A = beta_prior + visitors_to_A - conversions_from_A;
+    var alpha_posterior_A = alpha_prior + conversions_from_A;
+    var beta_posterior_A = beta_prior + visitors_to_A - conversions_from_A;
     
-    alpha_posterior_B = alpha_prior + conversions_from_B;
-    beta_posterior_B = beta_prior + visitors_to_B - conversions_from_B;
+    var alpha_posterior_B = alpha_prior + conversions_from_B;
+    var beta_posterior_B = beta_prior + visitors_to_B - conversions_from_B;
     
+    // Calculate which variant has a higher conversion rate
+    compare_conversion_probability(alpha_posterior_A, beta_posterior_A, alpha_posterior_B, beta_posterior_B);
     
     // set the dimensions and margins of the graph
-    var margin = {top: 30, right: 30, bottom: 30, left: 50},
+    var margin = {top: 10, right: 10, bottom: 30, left: 30},
         width = 460 - margin.left - margin.right,
         height = 300 - margin.top - margin.bottom;
 
@@ -78,6 +113,7 @@ looker.plugins.visualizations.add({
     var max_Y = 0;
     var max_X = 0;
     
+    // Draw beta distributions
     for (i = 0; i <= 1; i += 0.01) {
         var pdf_beta_A = jStat.beta.pdf(i, alpha_posterior_A, beta_posterior_A);
         var pdf_beta_B = jStat.beta.pdf(i, alpha_posterior_B, beta_posterior_B);
@@ -146,8 +182,8 @@ looker.plugins.visualizations.add({
     // Handmade legend
     svg.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
     svg.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
-    svg.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
-    svg.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 320).attr("y", 30).text(variant_A_label).style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 320).attr("y", 60).text(variant_B_label).style("font-size", "15px").attr("alignment-baseline","middle")
 
     doneRendering()
   }
